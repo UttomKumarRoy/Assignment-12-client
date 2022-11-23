69dc415a91efe9c5f230a39d0f67ec3c15b456b8
Uttom <uttom.teacher.gps2@gmail.com>
2022-11-24 01:38:18 +0600

    Advertised section added

--- src/Components/Main/Home.js
+++ src/Components/Main/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Advertisement from './Advertisement';
+import Banner from './Banner';
 import Footer from './Footer';
 
 const Home = () => {
     return (
         <div>
             I am home
+            <Banner></Banner>
+            <Advertisement></Advertisement>
             <Footer></Footer>
         </div>
     );
